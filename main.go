a50d5c7a21ef47e75bd912684bc0ff39f11c43ba
Derrick Yoo <derrickyoo@gmail.com>
2026-04-03 20:34:38 -0700

    Add flag

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"time"
@@ -9,6 +10,10 @@ import (
 )
 
 func main() {
+	var port int
+	flag.IntVar(&port, "port", 8080, "go backend server port")
+	flag.Parse()
+
 	app, err := app.NewApplication()
 	if err != nil {
 		panic(err)
@@ -17,13 +22,13 @@ func main() {
 	http.HandleFunc("/health", HealthCheck)
 
 	server := &http.Server{
-		Addr:         ":8080",
+		Addr:         fmt.Sprintf(":%d", port),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 
-	app.Logger.Println("Application is running")
+	app.Logger.Printf("Server is running on port %d", port)
 	err = server.ListenAndServe()
 	if err != nil {
 		app.Logger.Fatal(err)
