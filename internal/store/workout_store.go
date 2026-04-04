2d685902b68b08d7d5e86c430aa21c1eefd49711
Derrick Yoo <derrickyoo@gmail.com>
2026-04-04 11:21:06 -0700

    TODO GetWorkoutByID

--- internal/store/workout_store.go
+++ internal/store/workout_store.go
@@ -77,3 +77,9 @@ func (pg *PostgresWorkoutStore) CreateWorkout(workout *Workout) (*Workout, error
 
 	return workout, nil
 }
+
+// TODO
+func (pg *PostgresWorkoutStore) GetWorkoutByID(id int64) (*Workout, error) {
+	workout := &Workout{}
+	return workout, nil
+}
