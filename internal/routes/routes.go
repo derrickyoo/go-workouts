a84b997970d195c592b4122d71b3c49a88e33ffa
Derrick Yoo <derrickyoo@gmail.com>
2026-04-04 21:23:27 -0700

    add delete workout by ID

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -10,8 +10,11 @@ func SetupRoutes(app *app.Application) *chi.Mux {
 	r := chi.NewRouter()
 
 	r.Get("/health", app.HealthCheck)
+
 	r.Get("/workouts/{id}", app.WorkoutHandler.HandleGetWorkoutByID)
 	r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
+	r.Put("/workouts/{id}", app.WorkoutHandler.HandleUpdateWorkoutByID)
+	r.Delete("/workouts/{id}", app.WorkoutHandler.HandleDeleteWorkoutByID)
 
 	return r
 }
